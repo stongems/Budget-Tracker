a28655d063f7e5abb97bbe04f0e9d19746e7d61b
St.Onge <stonge.ms@gmail.com>
2021-07-27 22:17:19 -0700

    update

--- server.js
+++ server.js
@@ -10,8 +10,19 @@ const app = express();
 app.use(logger("dev"));
 
 app.use(compression());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
